afee31385a6102ab71b3c84157fe187dfb25f4cf
Dee Caulcrick <deecaulcrick@gmail.com>
2023-08-21 12:46:25 +0100

    adjusted media queries

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
 
   return (
     <div className="hero">
-      <h1 className='heroText'>What is your act of <br />kindness today?</h1>
+      <h1 className='heroText'>What is your act of kindness today?</h1>
       <p>Select from various categories, receive daily kindness suggestions with a click, spread joy, and share the experience with others.</p>
     </div>
   )
